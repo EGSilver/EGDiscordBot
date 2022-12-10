93c1d6a9fdddd064614500cd61484182328dbec8
Dean Vervaeck <dean.vervaeck@outlook.com>
2022-12-11 00:52:50 +0100

    implemented help, acces to java solutions

--- src/main/java/discordbot/bot.java
+++ src/main/java/discordbot/bot.java
@@ -10,14 +10,17 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.time.Duration;
 import java.util.Objects;
+import java.util.Scanner;
 
 
 public class bot extends ListenerAdapter {
     private static final String BOT_TOKEN = "";
 
-    public static void main(String[] arguments) throws Exception {
+    public static void main(String[] arguments) throws FileNotFoundException {
         JDA api = JDABuilder.createLight(BOT_TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
                 .addEventListeners(new bot())
                 .build();
@@ -31,34 +34,66 @@ public class bot extends ListenerAdapter {
         Duration thirtySeconds = Duration.ofSeconds(30);
         Member member = message.getMember();
         MessageChannel channel = event.getChannel();
-
         if (event.getAuthor().isBot()) return;
 
         if (content.toLowerCase().equals("!ping") && !event.getAuthor().isBot()) {
             channel.sendMessage("Pong!").queue();
         }
-
         if (content.toLowerCase().equals("!marco") && !event.getAuthor().isBot()) {
             channel.sendMessage("!Polo").queue();
         } else if (content.toLowerCase().equals("!polo")) {
             channel.sendMessage("!Marco").queue();
         }
-
         if (content.toLowerCase().equals("hi") || content.toLowerCase().equals("hello") || content.toLowerCase().equals("hey")) {
             System.out.println("Welcome message");
             System.out.println(message.getMember().getNickname());
             channel.sendMessage("Hallo " + nickname).queue();
         }
-
         if (content.toLowerCase().contains("fuck") || content.toLowerCase().contains("shit")) {
             message.getMember().timeoutFor(Duration.ofSeconds(30)).queue();
             channel.sendMessage(message.getMember().getEffectiveName() + " You said the word " + message + " You have been timed out for " + thirtySeconds).queue();
         }
-        if (content.startsWith("!taak")) {
-            channel.sendMessage("Welk vak?\n1. Java\n2. NoSQL\n3. Project\n4. UI").queue();
+        if (content.startsWith("!oplossing") || content.startsWith("!taak")) {
+            channel.sendMessage("typ meteen de naam van de taak, of kies Welk vak?\n1. Java\n2. NoSQL\n3. Project\n4. UI\n").queue();
             event.getJDA().addEventListener(new taakListener(channel, member.getUser()));
         }
-
+        if (content.toLowerCase().equals("!help")) {
+            int lineNumber = 1;
+            File help = new File("G:/Git/EGDiscordBot/src/JavaTaken/help.txt");
+            System.out.println("help file Loaded.");
+            Scanner txtIn = null;
+            try {
+                txtIn = new Scanner(help);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            while (txtIn.hasNextLine()) {
+                System.out.println(txtIn);
+                String line = txtIn.nextLine();
+                String s = line.replace("\n","");
+                channel.sendMessage("`" + s + "`").queue();
+            }
+        }
+        if (content.toLowerCase().equals("quinten is")) {
+            System.out.println("Quint");
+            System.out.println(message.getMember().getNickname());
+            channel.sendMessage("Quinten is kebab").queue();
+        } else if (content.toLowerCase().equals("!kebab")) {
+            System.out.println(message.getMember().getNickname());
+            channel.sendMessage("Kebab is Quinten.").queue();
+        }
+        if (content.toLowerCase().equals("jonathan is")) {
+            channel.sendMessage("Jonathan is de beste docent op school!").queue();
+        }
+        if (content.toLowerCase().equals("owen is")) {
+            channel.sendMessage(":zebra:").queue();
+        }
+        if (content.toLowerCase().equals("sander is")) {
+            channel.sendMessage(":sleeping_accommodation:").queue();
+        }
+        if (content.toLowerCase().equals("dean is") && !message.getAuthor().getName().equals("Netherspite")) {
+            channel.sendMessage(message.getMember().getNickname() + "You need to shut the fuck up").queue();
+        }
         System.out.println("Event Registered");
     }
 }
--- src/main/java/discordbot/taakListener.java
+++ src/main/java/discordbot/taakListener.java
@@ -5,6 +5,10 @@ import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class taakListener extends ListenerAdapter {
     private final long channelId, authorId;
 
@@ -20,23 +24,85 @@ public class taakListener extends ListenerAdapter {
         String content = event.getMessage().getContentRaw();
         if (content.equals("0")) {
             event.getJDA().removeEventListener(this); // stops listening.
+        } if (event.getAuthor().getIdLong() == authorId && content.equals("1") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("java")) {
+            channel.sendMessage("Welke oplossing van Java?").queue();
         }
-        if (event.getAuthor().getIdLong() == authorId && content.equals("1") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("java")) {
-            channel.sendMessage("Welke taak van Java?").queue();
+        if (event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("spaargeld") || content.toLowerCase().contains("while oef7")) {
+            int lineNumber = 1;
+            File spaargeld = new File("G:/Git/EGDiscordBot/src/JavaTaken/spaargeld.txt");
+            System.out.println("Oef7 Spaargeld Loaded.");
+            Scanner txtIn = null;
+            try {
+                txtIn = new Scanner(spaargeld);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            while (txtIn.hasNextLine()) {
+                System.out.println(txtIn);
+                String line = txtIn.nextLine();
+                channel.sendMessage("`" + line + "`").queue();
+                event.getJDA().removeEventListener(this);
+            }
+        } else if (event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("optellenmetrest") || content.toLowerCase().contains("while oef6")) {
+            File whileOef6 = new File("G:/Git/EGDiscordBot/src/JavaTaken/Oefening6GetalOptellenMetRest.txt");
+            System.out.println("Oef6 optellenmetrest Loaded.");
+            Scanner txtIn = null;
+            try {
+                txtIn = new Scanner(whileOef6);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            while (txtIn.hasNextLine()) {
+                System.out.println(txtIn);
+                String line = txtIn.nextLine();
+                channel.sendMessage("`" + line + "`").queue();
+                event.getJDA().removeEventListener(this);
+            }
+        } else if (event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("gemenedeler") || content.toLowerCase().contains("while oef5")) {
+            File gemeneDeler = new File("G:/Git/EGDiscordBot/src/JavaTaken/Oefening5GrootstGemeneDeler.txt");
+            System.out.println("Oef5 GemeneDeler Loaded.");
+            Scanner txtIn = null;
+            try {
+                txtIn = new Scanner(gemeneDeler);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            while (txtIn.hasNextLine()) {
+                System.out.println(txtIn);
+                String line = txtIn.nextLine();
+                channel.sendMessage("`" + line + "`").queue();
+                event.getJDA().removeEventListener(this);
+            }
+        } if (event.getAuthor().
+
+                getIdLong() == authorId && content.equals("2") || event.getAuthor().
+
+                getIdLong() == authorId && content.toLowerCase().
+
+                contains("nosql")) {
+            channel.sendMessage("Welke oplossing van NoSQL?").queue();
             event.getJDA().removeEventListener(this);
-        } else if (event.getAuthor().getIdLong() == authorId && content.equals("2") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("nosql")) {
-            channel.sendMessage("Welke taak van NoSQL?").queue();
+        } if (event.getAuthor().
+
+                getIdLong() == authorId && content.equals("3") || event.getAuthor().
+
+                getIdLong() == authorId && content.toLowerCase().
+
+                contains("project")) {
+            channel.sendMessage("Welke oplossing van Project?").queue();
             event.getJDA().removeEventListener(this);
-        } else if (event.getAuthor().getIdLong() == authorId && content.equals("3") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("project")) {
-            channel.sendMessage("Welke taak van Project?").queue();
-            event.getJDA().removeEventListener(this);
-        } else if (event.getAuthor().getIdLong() == authorId && content.equals("4") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("ui")) {
-            channel.sendMessage("Welke taak van UI?").queue();
+        } if (event.getAuthor().
+
+                getIdLong() == authorId && content.equals("4") || event.getAuthor().
+
+                getIdLong() == authorId && content.toLowerCase().
+
+                contains("ui")) {
+            channel.sendMessage("Welke oplossing van UI?").queue();
             event.getJDA().removeEventListener(this);
         } else {
-            channel.sendMessage("Ok dan niet.").queue();
             event.getJDA().removeEventListener(this);
-
         }
     }
 }
+
