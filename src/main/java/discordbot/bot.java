df6316f32485625f5a9385b6b809ac9eb2f1fd41
Dean Vervaeck <dean.vervaeck@outlook.com>
2022-12-10 21:53:33 +0100

    implemented marco polo & timeout example

--- src/main/java/discordbot/bot.java
+++ src/main/java/discordbot/bot.java
@@ -17,30 +17,42 @@ public class bot extends ListenerAdapter {
     private static final String BOT_TOKEN = "";
 
     public static void main(String[] arguments) throws Exception {
-        JDA api =   JDABuilder.createLight(BOT_TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
+        JDA api = JDABuilder.createLight(BOT_TOKEN, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
                 .addEventListeners(new bot())
                 .build();
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.getAuthor().isBot()) return;
         Message message = event.getMessage();
         String content = message.getContentRaw();
-        if (content.equals("!ping")) {
-            MessageChannel channel = event.getChannel();
+        String nickname = Objects.requireNonNull(message.getMember()).getNickname();
+        Duration thirtySeconds = Duration.ofSeconds(30);
+        MessageChannel channel = event.getChannel();
+
+        if (event.getAuthor().isBot()) return;
+
+        if (content.toLowerCase().equals("!ping") && !event.getAuthor().isBot()) {
             channel.sendMessage("Pong!").queue();
         }
-        if (content.toLowerCase().equals("hey") || content.toLowerCase().equals("hallo")) {
-            System.out.println("Welcome message");
-            Message messageNaught = event.getMessage();
-            String nickname = Objects.requireNonNull(messageNaught.getMember()).getNickname();
-            String contentNaught = message.getContentRaw();
-            MessageChannel channelNaught = event.getChannel();
-            channelNaught.sendMessage("Hallo " + nickname).queue();
 
-
+        if (content.toLowerCase().equals("!marco") && !event.getAuthor().isBot()) {
+            channel.sendMessage("!Polo").queue();
+        } else if (content.toLowerCase().equals("!polo")) {
+            channel.sendMessage("!Marco").queue();
         }
+
+        if (content.toLowerCase().contains("hi") || content.toLowerCase().contains("hello") || content.toLowerCase().contains("hey")) {
+            System.out.println("Welcome message");
+            System.out.println(message.getMember().getNickname());
+            channel.sendMessage("Hallo " + nickname).queue();
+        }
+
+        if (content.toLowerCase().contains("fuck") || content.toLowerCase().contains("shit")) {
+            message.getMember().timeoutFor(Duration.ofSeconds(30)).queue();
+            channel.sendMessage(message.getMember().getEffectiveName() + " You said the word " + message + " You have been timed out for " + thirtySeconds).queue();
+        }
+
         System.out.println("Event Registered");
     }
 }
