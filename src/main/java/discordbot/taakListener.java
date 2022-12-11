a5ddf657a4569a0f003ddc20f77d78c05fb959d2
Dean Vervaeck <dean.vervaeck@outlook.com>
2022-12-11 13:29:58 +0100

    Really Fixed a bug where eventListener would not focus on original message author, added a few more solutions to the java directory

--- src/main/java/discordbot/taakListener.java
+++ src/main/java/discordbot/taakListener.java
@@ -1,6 +1,7 @@
 package discordbot;
 
 import javafx.stage.DirectoryChooser;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -25,15 +26,17 @@ public class taakListener extends ListenerAdapter {
         if (event.getAuthor().isBot()) return; // stops the bots from responding to itself.
         DirectoryInfo scan = new DirectoryInfo();
         MessageChannel channel = event.getChannel();
+        Message message = event.getMessage();
         String content = event.getMessage().getContentRaw();
         if (content.equals("0")) {
             event.getJDA().removeEventListener(this); // stops listening.
-        }
-        if (event.getAuthor().getIdLong() == authorId && content.equals("1") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("java")) {
+        } else if (event.getAuthor().getIdLong() == authorId || event.getMessage().getMentions() == message.getMentions())  {
+            System.out.println("taakListener stopped");
+            event.getJDA().removeEventListener(this);
+        } else if (event.getAuthor().getIdLong() == authorId && content.equals("1") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("java")) {
             channel.sendMessage("Welke oplossing van Java? Kies een nummer of typ te naam van de oplossing/taak").queue();
             scan.readDirectory(event);
-        }
-        if (event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("spaargeld") || content.equals("7") || content.toLowerCase().contains("spaargeld")) {
+        } else if (event.getAuthor().getIdLong() == authorId && content.toLowerCase().contains("spaargeld") || content.equals("7") || content.toLowerCase().contains("spaargeld")) {
             int lineNumber = 1;
             File spaargeld = new File("G:/Git/EGDiscordBot/src/JavaTaken/7. Oefening7Spaargeld.txt");
             System.out.println("Oef7 Spaargeld Loaded.");
@@ -79,21 +82,15 @@ public class taakListener extends ListenerAdapter {
                 channel.sendMessage("`" + line + "`").queue();
                 event.getJDA().removeEventListener(this);
             }
-        }
-        if (event.getAuthor().getIdLong() == authorId && content.equals("2") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().
+        } else if (event.getAuthor().getIdLong() == authorId && content.equals("2") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().
                 contains("nosql")) {
             channel.sendMessage("Welke oplossing van NoSQL?").queue();
-        }
-        if (event.getAuthor().getIdLong() == authorId && content.equals("3") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().
+        } else if (event.getAuthor().getIdLong() == authorId && content.equals("3") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().
                 contains("project")) {
             channel.sendMessage("Welke oplossing van Project?").queue();
-        }
-        if (event.getAuthor().getIdLong() == authorId && content.equals("4") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().
+        } else if (event.getAuthor().getIdLong() == authorId && content.equals("4") || event.getAuthor().getIdLong() == authorId && content.toLowerCase().
                 contains("ui")) {
             channel.sendMessage("Welke oplossing van UI?").queue();
-        } else if (event.getAuthor().getIdLong() == authorId) {
-            System.out.println("taakListener stopped");
-            event.getJDA().removeEventListener(this);
         }
     }
 }
